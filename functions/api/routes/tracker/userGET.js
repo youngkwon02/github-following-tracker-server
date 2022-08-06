3e3597ac58a9f48ec8e6c7d8ae73e2aa9a4f40fc
youngkwon02 <youngkwon02@naver.com>
2022-08-07 00:04:10 +0900

    [CHORE] npm audit

--- functions/api/routes/tracker/userGET.js
+++ functions/api/routes/tracker/userGET.js
@@ -1,30 +1,11 @@
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
-const users = require('../../../dbMockup/user');
+const fetchGET = require('../../../lib/fetchGET');
+const githubHost = require('../../../lib/githubHost');
 
 module.exports = async (req, res) => {
-  // destructuring assignment
-  // 비구조화 할당
-  const { email, name, password } = req.body;
-
-  // request body가 잘못됐을 때
-  if (!email || !name || !password) {
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-  }
-
-  // 해당 email을 가진 유저가 이미 있을 때
-  const alreadyUser = users.filter((obj) => obj.email === email).length > 0;
-  if (alreadyUser) {
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_EMAIL));
-  }
-
-  const newUser = {
-    id: users.length + 1,
-    name,
-    password,
-    email,
-  };
-
-  res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.CREATED_USER, newUser));
+  const { userId } = req.params;
+  const githubUrl = githubHost(userId);
+  console.log(fetchGET(githubUrl));
 };
